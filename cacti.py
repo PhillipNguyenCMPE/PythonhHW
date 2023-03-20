dce4b8ad6031c00ba36767d4e9a2b87100d9556d
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-03-20 12:14:24 -0700

    yooo

--- cacti.py
+++ cacti.py
@@ -11,48 +11,49 @@ def cacti_number(plot):
             break
         for y in range(row):
             if plot[x][y] == 0:
-                if (x > 0) and (x < len(plot)-1) and (y > 0) and (y < row-1):
-                    if ((plot[x-1][y] == 0) and (plot[x][y-1] == 0) and (plot[x+1][y] == 0) and (plot[x][y+1]) == 0):
-                        plot[x][y] = 1
-                        #print(x, ' ',y)
                 if (x == 0):
                     if (y == 0):
                         if (plot[x+1][y]) == 0 and (plot[x][y+1] == 0):
                             plot[x][y] = 1
+			    cacti = cacti + 1
                             #print(x, ' ',y)
                     elif (y==row-1):
                         if (plot[x+1][y]) == 0 and (plot[x][y-1] == 0):
                             plot[x][y] = 1
+			    cacti = cacti + 1
                             #print(x, ' ',y)
                     else:
                         if (plot[x+1][y]) == 0 and (plot[x][y-1] == 0) and (plot[x][y+1] == 0):
                             plot[x][y] = 1
+			    cacti = cacti + 1
                             #print(x, ' ',y)
                 if (x == len(plot)-1):
                     if (y == 0):
                         if(plot[x-1][y] == 0) and (plot[x][y+1] == 0):
                             plot[x][y] = 1
+			    cacti = cacti + 1
                             #print(x, ' ',y)
                     elif (y == row-1):
                         if (plot[x-1][y]) == 0 and (plot[x][y-1] == 0):
                             plot[x][y] = 1
+			    cacti = cacti + 1
                             #print(x, ' ',y)
                     else:
                         if (plot[x-1][y] == 0) and (plot[x][y+1] == 0) and (plot[x][y-1] == 0):
                             plot[x][y] = 1
+			    cacti = cacti + 1
                             #print(x, ' ',y)
                 if (x > 0) and (x < len(plot)-1):
                     if y == 0:
                         if (plot[x+1][y] == 0) and (plot[x-1][y] == 0) and (plot[x][y+1] == 0):
                             plot[x][y] = 1
+			    cacti = cacti + 1
                     elif (y == row-1):
                         if ((plot[x+1][y] == 0) and (plot[x-1][y] == 0) and (plot[x][y-1] == 0)):
                             plot[x][y] = 1
+			    cacti = cacti + 1
                     else:
                         if ((plot[x+1][y] == 0) and (plot[x-1][y] == 0) and (plot[x][y+1] == 0) and (plot[x][y-1] == 0)):
                             plot[x][y] = 1
-    for x in plot:
-        for y in x:
-            if y == 1:
-                cacti = cacti + 1
+			    cacti = cacti + 1
     return cacti
--- sort.py
+++ sort.py
@@ -1,4 +1,5 @@
 def sort_dictionary(d1):
+	reversed = []
 	sorted_d1=sorted(d1.keys(), key = lambda key:d1[key][1])
 	for key in sorted_d1:
  		temp=(key,d1[key][0])
