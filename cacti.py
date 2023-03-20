0813f56d4577e374ddd913e04d8807390bbf4fa1
PhillipNguyenCMPE <125084184+PhillipNguyenCMPE@users.noreply.github.com>
2023-03-20 12:41:09 -0700

    Update cacti.py

--- cacti.py
+++ cacti.py
@@ -1,57 +1,43 @@
 def cacti_number(plot):
     total = 0
     cacti = 0
-    
     for i in plot:
         for j in i:
             total = total + 1
-            
     row=int(total/len(plot))
     
     for x in range(len(plot)):
         if(len(plot)) == 1:
             plot[x] = 1
-	    cacti = cacti + 1
             break
         for y in range(row):
             if plot[x][y] == 0:
-                if (x > 0) and (x < len(plot)-1) and (y > 0) and (y < row-1):
-                    if ((plot[x-1][y] == 0) and (plot[x][y-1] == 0) and (plot[x+1][y] == 0) and (plot[x][y+1]) == 0):
-                        plot[x][y] = 1
-                        #cacti = cacti + 1
-                        #print(x, ' ',y)
                 if (x == 0):
                     if (y == 0):
                         if (plot[x+1][y]) == 0 and (plot[x][y+1] == 0):
                             plot[x][y] = 1
                             cacti = cacti + 1
-                            #print(x, ' ',y)
                     elif (y==row-1):
                         if (plot[x+1][y]) == 0 and (plot[x][y-1] == 0):
                             plot[x][y] = 1
                             cacti = cacti + 1
-                            #print(x, ' ',y)
                     else:
                         if (plot[x+1][y]) == 0 and (plot[x][y-1] == 0) and (plot[x][y+1] == 0):
                             plot[x][y] = 1
                             cacti = cacti + 1
-                            #print(x, ' ',y)
                 if (x == len(plot)-1):
                     if (y == 0):
                         if(plot[x-1][y] == 0) and (plot[x][y+1] == 0):
                             plot[x][y] = 1
                             cacti = cacti + 1
-                            #print(x, ' ',y)
                     elif (y == row-1):
                         if (plot[x-1][y]) == 0 and (plot[x][y-1] == 0):
                             plot[x][y] = 1
                             cacti = cacti + 1
-                            #print(x, ' ',y)
                     else:
                         if (plot[x-1][y] == 0) and (plot[x][y+1] == 0) and (plot[x][y-1] == 0):
                             plot[x][y] = 1
                             cacti = cacti + 1
-                            #print(x, ' ',y)
                 if (x > 0) and (x < len(plot)-1):
                     if y == 0:
                         if (plot[x+1][y] == 0) and (plot[x-1][y] == 0) and (plot[x][y+1] == 0):
@@ -61,7 +47,6 @@ def cacti_number(plot):
                         if ((plot[x+1][y] == 0) and (plot[x-1][y] == 0) and (plot[x][y-1] == 0)):
                             plot[x][y] = 1
                             cacti = cacti + 1
-
                     else:
                         if ((plot[x+1][y] == 0) and (plot[x-1][y] == 0) and (plot[x][y+1] == 0) and (plot[x][y-1] == 0)):
                             plot[x][y] = 1
