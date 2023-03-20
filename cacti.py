b823540e395af243ade2ce348767bad6f34d2a1f
PhillipNguyenCMPE <125084184+PhillipNguyenCMPE@users.noreply.github.com>
2023-03-20 12:43:37 -0700

    Update cacti.py

--- cacti.py
+++ cacti.py
@@ -8,7 +8,7 @@ def cacti_number(plot):
     
     for x in range(len(plot)):
         if(len(plot)) == 1:
-            plot[x] = 1
+            cacti = cacti + 1
             break
         for y in range(row):
             if plot[x][y] == 0:
