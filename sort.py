eed122a047d24f8c9877b2d9f5fb1a32adc20e92
PhillipNguyenCMPE <125084184+PhillipNguyenCMPE@users.noreply.github.com>
2023-03-20 12:53:54 -0700

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,7 +1,7 @@
 def sort_dictionary(d1):
-	reversed = []
-	sorted_d1 = sorted(d1.keys(), key=lambda key:d1[key][1])
+	reversed=[]
+	sorted_d1=sorted(d1.keys(), key=lambda key:d1[key][1])
 	for key in sorted_d1:
- 		temp=(key,d1[key][0])
-    		reversed.append(temp)
-	return reversed
+		temp=(key,d1[key][0])
+		reversed.append(temp)
+		return reversed
