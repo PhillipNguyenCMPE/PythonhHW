819799b59052be9be6dba125eb77986669dad4a4
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-03-20 09:52:18 -0700

    number1

---
+++ palin.py
@@ -0,0 +1,9 @@
+def palin(someList):
+    status = True
+    if len(someList) <= 0:
+        return False;
+    else:
+        for x in range(int(len(someList)/2)):
+            if someList[x] != someList[len(someList)-x-1]:
+                status = False
+        return status
