a293d14b5166eb82a0ef46207607c4635dddc7a9
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-03-20 12:23:23 -0700

    yo

--- sort.py
+++ sort.py
@@ -1,7 +1,9 @@
 def sort_dictionary(d1):
 	reversed = []
-	sorted_d1=sorted(d1.keys(), key = lambda key:d1[key][1])
+	sorted_d1 = sorted(d1.keys(), key = lambda key:d1[key][1])
+
 	for key in sorted_d1:
  		temp=(key,d1[key][0])
     		reversed.append(temp)
+
 	return sorted_d1
