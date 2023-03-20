b94e2c9cf761e26c5b911303b70c5e6d0078979c
PhillipNguyenCMPE <125084184+PhillipNguyenCMPE@users.noreply.github.com>
2023-03-20 16:04:13 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from double import double
+from log import timestamp
+
+def double(func):
+    func()
+    print('''Let's try again!''')
+    func()
+def timestamp(func):
+    time.ctime()
+    func()
