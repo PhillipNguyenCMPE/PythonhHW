ab0ba53fb4282a226ae7a92ac524eea307e78742
PhillipNguyenCMPE <125084184+PhillipNguyenCMPE@users.noreply.github.com>
2023-03-20 21:06:25 -0700

    Update log.py

--- log.py
+++ log.py
@@ -1,10 +1,7 @@
-from log import timestamp
-@timestamp
-def hi():
-	print('hi')
-def main():
-	hi()
+
 def timestamp(func):
-    time.ctime()
-    func()
+	def wrapper():
+		time.ctime()
+		func()
+		
 
