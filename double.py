13ddcd39a5940c22b769a566341578ce51583e4d
PhillipNguyenCMPE <125084184+PhillipNguyenCMPE@users.noreply.github.com>
2023-03-20 21:03:00 -0700

    Update double.py

--- double.py
+++ double.py
@@ -1,13 +1,7 @@
-from double import double
-@timestamp
-def greet():
-	print('Hello World!')
-
-def main():
-	greet()
-
 def double(func):
-    func()
-    print('''Let's try again!''')
-    func()	
+	def wrapper():
+		func()
+		print('''Let's try that again!''')
+		func()
+	return wrapper
 	
